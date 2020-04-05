8505ca8b95d244dde76906183395dda676b1382f
Pedro Dias <petermdias@gmail.com>
2020-04-05 22:00:49 +0100

    version bump

--- examples/startvm.js
+++ examples/startvm.js
@@ -4,12 +4,12 @@ var Firecracker = require('../lib/firecracker');
 var firecracker = new Firecracker({ socketPath: '/tmp/firecracker.socket' });
 
 firecracker.bootSource({
-  'kernel_image_path': os.tmpdir() + '/images/hello-vmlinux.bin',
+  'kernel_image_path': os.tmpdir() + '/hello-vmlinux.bin',
   'boot_args': 'console=ttyS0 reboot=k panic=1 pci=off'
 }).then(function (data) {
   var drive = firecracker.drive('rootfs');
   return drive.updatePreboot({
-    'path_on_host': os.tmpdir() + '/images/hello-rootfs.ext4',
+    'path_on_host': os.tmpdir() + '/hello-rootfs.ext4',
     'is_root_device': true,
     'is_read_only': false
   });
