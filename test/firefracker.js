f4957e3f169fdc91eb7d8a2a6085039c8ba1baa3
Pedro Dias <petermdias@gmail.com>
2020-04-05 22:59:07 +0100

    mocha and promises

--- test/firefracker.js
+++ test/firefracker.js
@@ -5,34 +5,31 @@ var firecracker = require('./helper').firecracker;
 describe('#firecracker', function () {
 
   describe('#images', function () {
-    it('should download kernel & filesystem images', function (done) {
+    it('should download kernel & filesystem images', function () {
       var kernelImg = 'https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin';
       var rootImg = 'https://s3.amazonaws.com/spec.ccfc.min/img/hello/fsfiles/hello-rootfs.ext4';
 
-      firecracker.downloadImage(kernelImg, os.tmpdir() + '/hello-vmlinux.bin').then(function () {
+      return firecracker.downloadImage(kernelImg, os.tmpdir() + '/hello-vmlinux.bin').then(function () {
         console.log('Kernel image downloaded!');
         return firecracker.downloadImage(rootImg, os.tmpdir() + '/hello-rootfs.ext4');
       }).then(function () {
         console.log('Filesystem image downloaded!');
-        done();
       }).catch(function (err) {
         console.log(err);
         expect(err).to.be.null;
-        done();
       });
     });
 
-    it('should get info', function(done) {
-      firecracker.info().then(function(data) {
+    it('should get info', function() {
+      return firecracker.info().then(function(data) {
         console.log(data);
         expect(data).to.be.ok;
-        done();
-      }).catch(function (err) {
+      }, function(err) {
         console.log(err);
         expect(err).to.be.null;
-        done();
       });
     });
+    
   });
 
 });
