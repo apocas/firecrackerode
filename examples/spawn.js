586dc31759472834e8255f15345cfe0cb9ef4da5
Pedro Dias <petermdias@gmail.com>
2020-04-05 21:36:23 +0100

    streams

---
+++ examples/spawn.js
@@ -0,0 +1,18 @@
+var Firecracker = require('../lib/firecracker');
+
+var firecracker = new Firecracker({ socketPath: '/tmp/firecracker.socket' });
+
+var streams = firecracker.spawn('/root/firecracker');
+
+process.on('SIGINT', function () {
+  console.log('Killing Firecracker...');
+  firecracker.kill();
+});
+
+streams.stdout.on('data', (data) => {
+  console.log(`Firecracker stdout:\n${data}`);
+});
+
+streams.stderr.on('data', (data) => {
+  console.log(`Firecracker stderr:\n${data}`);
+});
